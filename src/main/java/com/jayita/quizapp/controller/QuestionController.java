1e00fbe1822e728c9696f6365f00ea02bcf02a35
JayitaS <1217jayitasvjc@gmail.com>
2025-11-28 14:50:56 +0530

    initial_commit

---
+++ src/main/java/com/jayita/quizapp/controller/QuestionController.java
@@ -0,0 +1,32 @@
+package com.jayita.quizapp.controller;
+
+import com.jayita.quizapp.Question;
+import com.jayita.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+    @GetMapping("allQuestions")
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        return new ResponseEntity<>( questionService.getAllQuestions(), HttpStatus.OK);
+    }
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question ){
+        return questionService.addQuestion(question);
+    }
+}
---
+++ src/main/java/com/jayita/quizapp/dao/QuestionDao.java
@@ -0,0 +1,13 @@
+package com.jayita.quizapp.dao;
+
+import com.jayita.quizapp.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionDao extends JpaRepository<Question, Integer> {
+    List<Question> findByCategory(String category);
+
+}
